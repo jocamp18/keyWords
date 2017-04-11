92a5b78b25f730f24408564497a7f42f5c2d4d03
Diego Ocampo <jocamp18@eafit.edu.co>
2017-04-11 16:24:35 -0500

    Adding support for multiple-word search

--- controller.py
+++ controller.py
@@ -4,9 +4,16 @@ import sys
 def count_words(phrase):
   db = Mongo()
   words = []
+  i = 0
+  dic = {}
   for word in phrase.split():
-    words.append(db.search(word))
-  print(words)
+    word_aux = db.search(word)['file_paths']
+    for file in word_aux:
+      if file[0] not in dic:
+        dic[file[0]] = file[1]
+      else:
+        dic[file[0]] += file[1]
+  print(dic)
 
 if __name__ == "__main__":
   count_words(sys.argv[1])
