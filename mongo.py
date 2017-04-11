901ece366917706a3a1b2aedab8c1e394fbbf00c
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-04-11 15:03:23 -0500

    controller.py

--- mongo.py
+++ mongo.py
@@ -5,6 +5,9 @@ class Mongo():
     self.client = MongoClient()
     self.db = self.client.keyword
     self.indexes = self.db.indexes
-  
+
   def insert(self, word, file_paths):
     self.indexes.insert_one({"_id": word, "file_paths": file_paths})
+
+  def search(self, word):
+    return self.indexes.find_one({"_id": word})
