fe5029efcaad5e7ad04cd5cff568b97c47cd4b59
jyepesr1 <johanyepes@hotmail.com>
2017-04-18 20:33:41 -0500

    Creating List view

--- app/views.py
+++ app/views.py
@@ -4,3 +4,21 @@ from app import app, mongo
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+  if request.method == 'POST':
+    keywords = request.form['keyword']
+    language = request.form['language']
+    #paths = get_files(keywords, language)
+    paths = ["http://hola.com/hola", "http://chao.com/chao"]
+    return render_template('list.html', paths=paths)
+
+  return redirect(url_for('index'))
+
+@app.context_processor
+def utility_processor():
+  def get_name(filepath):
+    file_name = filepath.split('/')
+    return file_name[-1]
+  return dict(get_name=get_name)
