6f2409daf4e0502d0315b8c96f424618651ae9b8
jyepesr1 <johanyepes@hotmail.com>
2017-04-20 10:01:15 -0500

    Stemming in spanish or english

--- inverted_index.py
+++ inverted_index.py
@@ -1,40 +1,57 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
-from mongo import Mongo
+from langdetect import detect
+from langdetect import DetectorFactory
+#from mongo import Mongo
 import os
 
-ps = PorterStemmer()
-stop_words = stopwords.words('english')
+spanish_stemmer = SnowballStemmer('spanish')
+english_stemmer = SnowballStemmer('porter')
+spanish_stop_words = stopwords.words('spanish')
+english_stop_words = stopwords.words('english')
 tokenizer = RegexpTokenizer(r'\w+')
-db = Mongo()
+DetectorFactory.seed = 0
+#db = Mongo()
 
 def tokenize(line):
   stemmed_words = []
   words = tokenizer.tokenize(line)
-  for w in words:
-    if w not in stop_words:
-      stemmed_words.append(ps.stem(w))
+  if detect(line) == "es":
+    for w in words:
+      if w not in spanish_stop_words:
+        stemmed_words.append(spanish_stemmer.stem(w))
+  else:
+    for w in words:
+      if w not in english_stop_words:
+        stemmed_words.append(english_stemmer.stem(w))
+
   return stemmed_words
 
+
 class InvertedIndex(MRJob):
-  
+
   def mapper(self, key, line):
     words = tokenize(line)
     file_name = os.environ['mapreduce_map_input_file']
     for word in words:
-      yield word, file_name
+      yield (word, file_name),1
+
+  def combiner(self, pair, values):
+    yield pair[0], (pair[1], sum(values))
 
   def reducer(self, word, values):
     values = list(values)
-    result = dict.fromkeys(set(values), 0)
-    for document_path in values:
-      result[document_path] += 1
+    file_names, frec = zip(*values)
+    file_names = list(file_names)
+    result = dict.fromkeys(set(file_names), 0)
+    for file_name in values:
+      result[file_name[0]] += file_name[1]
     result = list(result.items())
-    db.insert(word, result)
+    #db.insert(word, result)
     yield word, result
-  
+
 if __name__ == '__main__':
   InvertedIndex.run()
